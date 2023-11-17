3f92873c848597ccd849e517bb93c14e8fc8e742
alexeyukrainets <qaaq.com.ua@gmail.com>
2023-11-17 14:57:34 +0100

    second commit

---
+++ src/main/java/tech/task/techtask/TechTaskApplication.java
@@ -0,0 +1,12 @@
+package tech.task.techtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechTaskApplication.class, args);
+    }
+}
